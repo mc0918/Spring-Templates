483149e21948919722da0917eceee2c9a7fb1d87
Michael Campbell <mcampbell0918@gmail.com>
2019-11-17 14:33:01 -0600

    eureka server

---
+++ eureka-server-template/src/main/java/com/example/eurekaservertemplate/EurekaServerTemplateApplication.java
@@ -0,0 +1,19 @@
+package com.example.eurekaservertemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaServerTemplateApplication {
+
+	/*
+	THIS APP IS GOOD TO GO, JUST RUN IT
+	 */
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerTemplateApplication.class, args);
+	}
+
+}
---
+++ eureka-server-template/src/test/java/com/example/eurekaservertemplate/EurekaServerTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaservertemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
