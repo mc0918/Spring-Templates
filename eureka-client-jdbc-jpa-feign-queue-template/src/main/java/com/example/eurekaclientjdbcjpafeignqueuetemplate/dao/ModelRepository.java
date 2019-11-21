5f345d11de03c695bcb598c94cc447a2bd2299bb
Michael Campbell <mcampbell0918@gmail.com>
2019-11-21 11:27:45 -0600

    added test template

--- eureka-client-jdbc-jpa-feign-queue-template/src/main/java/com/example/eurekaclientjdbcjpafeignqueuetemplate/dao/ModelRepository.java
+++ eureka-client-jdbc-jpa-feign-queue-template/src/main/java/com/example/eurekaclientjdbcjpafeignqueuetemplate/dao/ModelRepository.java
@@ -1,8 +1,9 @@
 package com.example.eurekaclientjdbcjpafeignqueuetemplate.dao;
 
+import com.example.eurekaclientjdbcjpafeignqueuetemplate.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ModelRepository extends JpaRepository<Object, Integer> {
+public interface ModelRepository extends JpaRepository<Model, Integer> {
 }
---
+++ eureka-client-jdbc-jpa-feign-queue-template/src/test/java/com/example/eurekaclientjdbcjpafeignqueuetemplate/service/ServiceLayerTest.java
@@ -0,0 +1,16 @@
+package com.example.eurekaclientjdbcjpafeignqueuetemplate.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ServiceLayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void shouldPutTestHere(){}
+}
